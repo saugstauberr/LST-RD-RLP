8a252b4231be9725b0556912c371313ccbfe7b53
Martin <57033944+saugstauberr@users.noreply.github.com>
2025-04-05 18:36:22 +0200

    Change keyboard to numeric and seperation
    Code seperated now every 3 chars and not via komma.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-function convert() {
-    var codes = document.getElementById("codes").value.split(',')
+function convert() {    
+    var codes = document.getElementById("codes").value.toString().match(/.{1,3}/g)
     var result = "";
 
    
 
-    for (const code of codes) {
+    for (const rawCode of codes) {
+        code = rawCode
         result = result + code + ": " + alarms_rlp[parseInt(code)] + "<br><br>"
     }
 
